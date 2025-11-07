4d2ef29268479f66a14e36be89bad228f92cb57f
Yosip Mike Colin <colinparrado@gmail.com>
2025-11-07 09:48:25 -0500

    User table view settings

--- src/services/requests/index.ts
+++ src/services/requests/index.ts
@@ -8,6 +8,14 @@ export const getPrograms = async (page: number, limit: number) => {
   ).data;
 };
 
+export const getUsers = async (page: number, limit: number) => {
+  return (
+    await axiosConfig.get<UserResponse>("/user", {
+      params: { page, limit },
+    })
+  ).data;
+};
+
 export const addProgramToUser = async (userId: string, programId: string) => {
   return (await axiosConfig.post(`/user/${userId}/programs/${programId}`)).data;
 };
